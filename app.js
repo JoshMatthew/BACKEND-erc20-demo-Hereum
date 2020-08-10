ab11a081907b1cdf724bccec64e6ba6be317ccb7
Josh Matthew Talplacido <joshmatthewtalplacido@gmail.com>
2020-08-10 13:45:24 +0800

    fixed cors

--- app.js
+++ app.js
@@ -21,13 +21,15 @@ db.once('open', function () {
 
 // Middlewares
 app.use(express.json())
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+app.use(cors())
+app.options('*', cors())
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", '*');
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//   next();
+// });
 
 
 // Routes
