d7318e5bd54165f090c51ff0ff644775ea109347
Josh Matthew Talplacido <joshmatthewtalplacido@gmail.com>
2020-08-10 13:15:05 +0800

    fixed cors

--- routes/routes.peewpeew.js
+++ routes/routes.peewpeew.js
@@ -1,20 +1,5 @@
 const express = require('express')
 const route = express.Router()
-const cors = require('cors')
-
-
-// Used to allow cors for specific sites
-const whitelist = ['https://hereum.herokuapp.com/', 'https://joshmatthew.github.io/PeewPeew/']
-
-const corsOptionsDelegate = function (req, callback) {
-  let corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
 
 // controllers 
 const { postFromPeewPeew, getFromPeewPeew } = require('../controllers/controllers.peewpeew')
@@ -24,14 +9,14 @@ const { postFromPeewPeew, getFromPeewPeew } = require('../controllers/controller
 * access: PUBLIC 
 * desc: sends the points to the database so hereum client can retrieve it
 */
-route.post('/points', cors(corsOptionsDelegate), postFromPeewPeew)
+route.post('/points', postFromPeewPeew)
 
 /*
 * method: GET
 * access: PUBLIC 
 * desc: retrieves the points from the database so hereum client can convert it to hre
 */
-route.get('/points/:acc/:claim', cors(corsOptionsDelegate), getFromPeewPeew)
+route.get('/points/:acc/:claim', getFromPeewPeew)
 
 
 
--- routes/routes.token.js
+++ routes/routes.token.js
@@ -1,20 +1,5 @@
 const express = require('express')
 const route = express.Router()
-const cors = require('cors')
-
-
-// Used to allow cors for specific sites
-const whitelist = ['https://hereum.herokuapp.com/', 'https://joshmatthew.github.io/PeewPeew/']
-
-const corsOptionsDelegate = function (req, callback) {
-  let corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
 
 
 // controllers 
@@ -25,6 +10,6 @@ const { getHereumContract } = require('../controllers/controllers.token')
 * access: PUBLIC 
 * desc: sends the smart contract to the client
 */
-route.get('/contract', cors(corsOptionsDelegate), getHereumContract)
+route.get('/contract', getHereumContract)
 
 module.exports = route
